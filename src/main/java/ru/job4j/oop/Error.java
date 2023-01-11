815de47446b61b6faf6a19edc4cf925a4e962f8c
Eduard <ebiryulin@gmail.com>
2023-01-11 17:37:43 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public void printInfo() {
+        System.out.println("Наличие ошибки: " + active);
+        System.out.println("Номер ошибки: " + status);
+        System.out.println("Содержание ошибки: " + message);
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(true, 5, "Unknown symbol");
+        error1.printInfo();
+        Error error2 = new Error(true, 7, "Add  new symbol");
+        error2.printInfo();
+
+    }
+}
