037a9523a66b7d0b8104fe8bfdfd320140d652b8
Eduard <ebiryulin@gmail.com>
2023-01-30 15:22:24 +0300

    9. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,7 +1,8 @@
 package ru.job4j.tracker;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
