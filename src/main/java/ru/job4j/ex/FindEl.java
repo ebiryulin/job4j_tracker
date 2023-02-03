0192de38310101565bceec1cefc29dc97b589481
Eduard <ebiryulin@gmail.com>
2023-02-03 12:18:46 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,29 +1,27 @@
 package ru.job4j.ex;
 
-import java.util.Arrays;
-
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int index = 0; index < value.length; index++) {
-            boolean found = Arrays.asList(value).contains(key);
-            if (!found) {
-                throw new ElementNotFoundException("Element " + key + " doesn't exist in array");
-            }
             if (value[index].equals(key)) {
                 rsl = index;
-                }
+                break;
+            }
+        }
+        if (rsl != -1) {
+            throw new ElementNotFoundException("Element " + key + " doesn't exist in array");
         }
         return rsl;
     }
 
-    public static void main(String[] args) {
-        String[] value = {"one", "two", "three"};
-        String key = "threee";
-        try {
-            indexOf(value, key);
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
+        public static void main(String[]args) {
+            String[] value = {"one", "two", "three"};
+            String key = "three";
+            try {
+                indexOf(value, key);
+            } catch (ElementNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
-}
