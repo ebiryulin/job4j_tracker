0089c0ab099729eec920830f8e09e1ebbc39e275
Eduard <ebiryulin@gmail.com>
2023-02-23 10:48:39 +0300

    9. Изменить программу Tracker из 2-го модуля [#10039 #394115]

--- src/main/java/ru/job4j/tracker/FindItemById.java
+++ src/main/java/ru/job4j/tracker/FindItemById.java
@@ -24,17 +24,4 @@ public class FindItemById implements UserAction {
         }
         return true;
     }
-
-    //@Override
-    //public boolean execute(Input input, Tracker tracker) {
-        //out.println("=== Find item by id ===");
-        //int id = input.askInt("Enter id: ");
-        //Item item = tracker.findById(id);
-        //if (item != null) {
-            //out.println(item);
-        //} else {
-            //out.println("Заявка с введенным id: " + id + " не найдена.");
-        //}
-        //return true;
-    //}
 }
